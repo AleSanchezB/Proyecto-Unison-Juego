8c99e18175c5abd320747ef6724bfd6f350d4cc9
Braulio Sanchez lap <sanchezbraulio542gmail.com>
2023-02-24 14:32:16 -0700

    Error en el display opciones

--- ProyectoJuegoUnison/InitMain.cpp
+++ ProyectoJuegoUnison/InitMain.cpp
@@ -12,6 +12,7 @@ using namespace std;
 ALLEGRO_DISPLAY* menu;
 ALLEGRO_BITMAP* icon = NULL;
 ALLEGRO_EVENT_QUEUE* queueE;
+ALLEGRO_EVENT_QUEUE* aux;
 ALLEGRO_FONT* fontInit;
 ALLEGRO_KEYBOARD_STATE keystate2;
 ALLEGRO_EVENT event;
@@ -31,10 +32,9 @@ void initMenu();
 void drawOptions();
 void initVars();
 void destroy();
+
 void initVars()
 {
-
-    menu = al_create_display(W, H);
     icon = al_load_bitmap("assets/IdleTam/frame01.png");
     fontInit = al_load_font("assets/fonts/Minecraft.ttf", 20, 0);
     al_set_window_title(menu, "Ventana Inicio");
@@ -43,14 +43,15 @@ void initVars()
 }
 
 void initMenu() {
+    menu = al_create_display(W, H);
     al_install_keyboard();
     al_install_mouse();
     al_init_image_addon();
     al_init_font_addon();
     al_init_ttf_addon();
     initVars(); //inicializa las variables
+    queueE = al_create_event_queue();
     al_set_display_icon(menu, icon);
-
     // Registrar del display como fuente de eventos
     al_register_event_source(queueE, al_get_display_event_source(menu));
     al_register_event_source(queueE, al_get_keyboard_event_source());
@@ -60,12 +61,9 @@ void initMenu() {
 int main(int argc, char** argv) {
     al_init();
     initMenu(); //crea la pantalla y registra los eventos    
-
     // Bucle del juego
     while (run) {
         al_wait_for_event(queueE, &event);
-        al_get_keyboard_state(&keystate2);
-
         // Dibujar el menú
         drawOptions();
         if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
@@ -90,11 +88,11 @@ int main(int argc, char** argv) {
                 }
                 if (x <= W / 2 + 30 && x >= W / 2 - 40 && y >= H / 2 && y <= H / 2 + 30)
                 {
+                    aux = queueE;
                     printf("Opciones");
-                    run = false;
-                    destroy();
-                    //ia();
-                    //animation();
+                    menuOptions();
+                    initVars();
+                    queueE = aux;
                 }
                 if (x <= W / 2 + 30 && x >= W / 2 - 40 && y >= H / 2 + 35 && y <= H / 2 + 55)
                 {
@@ -113,7 +111,6 @@ void destroy() {
     al_destroy_event_queue(queueE);
     al_uninstall_keyboard();
     al_uninstall_mouse();
-
 }
 void drawOptions() {
     al_clear_to_color(al_map_rgb(0, 0, 0));
