f4d05d376aafa73bbf8005b0a946f460805ba8c3
Braulio Sanchez lap <sanchezbraulio542gmail.com>
2023-04-07 11:59:18 -0700

    NO NECESARIO

--- ProyectoJuegoUnison/main.cpp
+++ ProyectoJuegoUnison/main.cpp
@@ -1,7 +1,40 @@
-#include "MainGame.h"
+//#include "MainGame.h"
+#include "Objetcs.h"
+#include <allegro5/allegro5.h>
+#include <allegro5/bitmap.h>
+#include <list>
 
 using namespace std;
 
+#include "Objetcs.h"
+#include "Personajes.h"
+
 int main(int argc, char** argv) {
-	MainGame main = MainGame();
+	//MainGame main = MainGame();
+	bool running = true;
+	al_init();
+	al_init_image_addon();
+	al_install_mouse();
+	ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
+	ALLEGRO_EVENT event;
+	ALLEGRO_DISPLAY* displayGame = al_create_display(800, 600);
+
+
+	al_register_event_source(queue, al_get_display_event_source(displayGame));
+	Objects* ob = new Objects();
+	while (running)
+	{
+		al_wait_for_event(queue, &event);
+		ALLEGRO_KEYBOARD_STATE keystate;
+		al_get_keyboard_state(&keystate);
+
+		if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+		{
+			running = false;
+		}
+		for (std::list<Objects*>::iterator it = instancias.begin(); it != instancias.end(); it++)
+		{
+			Objects* other = *it;
+		}
+	}
 }
